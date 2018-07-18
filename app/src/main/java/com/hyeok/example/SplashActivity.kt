9b6be869f23f0edfeba521dc00a7e8ccbd80b419
최혁재 <gurwo1007@gmail.com>
2018-07-18 15:59:10 +0900

    Add Twitter sdk Add Facebook, Google, Twitter Login Option in AuthUI Intent

--- app/src/main/java/com/hyeok/example/SplashActivity.kt
+++ app/src/main/java/com/hyeok/example/SplashActivity.kt
@@ -3,21 +3,58 @@ package com.hyeok.example
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.hyeok.example.auth.AuthActivity
+import java.util.*
 
 class SplashActivity : AppCompatActivity() {
+
+    private val RC_SIGN_IN : Int = 123
+    lateinit var mainIntent : Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val currentUser : FirebaseUser? = FirebaseAuth.getInstance()?.currentUser
+        mainIntent = Intent(this, AuthActivity::class.java)
         currentUser?.let {
-            val mainIntent : Intent = Intent(this, AuthActivity::class.java)
             startActivity(mainIntent)
+            finish()
         }.let {
+            val providers : List<AuthUI.IdpConfig> = Arrays.asList(
+                    AuthUI.IdpConfig.FacebookBuilder().build(),
+                    AuthUI.IdpConfig.GoogleBuilder().build(),
+                    AuthUI.IdpConfig.TwitterBuilder().build()
+            )
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .setTheme(R.style.Theme_AppCompat_Light_NoActionBar)
+                            .setLogo(R.drawable.ic_launcher_background)
+                            .build(),
+                    RC_SIGN_IN
+            )
+        }
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == RC_SIGN_IN) run {
+            val response: IdpResponse = IdpResponse.fromResultIntent(data)!!
+
+            if(resultCode == RESULT_OK) {
+                mainIntent.putExtra("userInfo", response)
+                startActivity(mainIntent)
+                finish()
+            }else{
+                Toast.makeText(this, response.error?.errorCode.toString(), Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
